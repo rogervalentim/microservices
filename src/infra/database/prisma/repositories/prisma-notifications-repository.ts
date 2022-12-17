39fc0aaf1255c5be00929bfe41a64633870c582b
rogervalentim <valentim.roger33@gmail.com>
2022-12-16 22:54:18 -0300

    new technical concepts

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,23 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { Notification } from '../../../../application/entities/notification';
-import { NotificationsRepository } from '../../../../application/repositories/notification-repository';
+import { Notification } from '@application/entities/notification';
+import { NotificationsRepository } from '@application/repositories/notification-repository';
 import { PrismaService } from '../prisma.services';
+import { PrismaNotificationMapper } from '../mappers/prisma-notifications-mappers';
 
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
   async create(notification: Notification): Promise<void> {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
     await this.prismaService.notification.create({
-        data: {
-            id: notification.id,
-            category: notification.category,
-            content: notification.content.value,
-            recipientId: notification.recipientId,
-            readAt: notification.readAt,
-            createdAt: notification.createdAt,
-            
-        }
-    })
+      data: raw,
+    });
   }
 }
